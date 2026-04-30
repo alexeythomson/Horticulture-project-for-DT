970786401c7746ad091bbe6e8f9372305523d5ab
Matte9i <alexey.thomson08@gmail.com>
2026-04-30 23:19:08 +1200

    halfway through classes and objects

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import json
 '''code to compare apple prices '''
+
+class Apple:
+    def __init__(self, weight, v_quality, i_quality, s_time):
+        self.weight = weight
+        self.v_quality = v_quality
+        self.i_quality = i_quality
+        self.s_time = s_time
+
 with open("resources/apples.json", 'r') as file:
     info = json.load(file)
-    print(info)
+
