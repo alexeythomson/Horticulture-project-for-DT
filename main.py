096eee4762822c6e1cc4b759df87a35a42b52db3
Matte9i <alexey.thomson08@gmail.com>
2026-05-02 23:52:44 +1200

    comments added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 '''code to show profitability '''
 
+#this makes a blueprint that all apples follow
+#this blueprint is attributed to the objects which are the species of apples
+#this makes the code future-proof if I add in more apple variety's
 class Apple:
     def __init__(self, name, weight, v_quality, i_quality, s_time):
         self.name = name
@@ -9,13 +12,18 @@ class Apple:
         self.i_quality = i_quality
         self.s_time = s_time
 
+#__repr__ is useful for seeing the attributes of the apple
+#without the __repr__ the code will print "<__main__.Apple object at 0x000...>"
     def __repr__(self):
-        return f"{self.name}, {self.weight} grams, {self.v_quality}, {self.i_quality}, {self.s_time}"
+        return (f"{self.name}, {self.weight} grams, "
+                f"{self.v_quality} out of 10, "
+                f"{self.i_quality} out of 10, {self.s_time} out of 10")
 
+#opens my file and by using 'with' it auto closes the file after doing the task
 with open("resources/apples.json", 'r') as f:
     apple_attributes = json.load(f)
 
-
+#creates an instance from the apple class for royal gala
 Royal_Gala = Apple(**apple_attributes['Royal_gala'])
 
 print(Royal_Gala)
