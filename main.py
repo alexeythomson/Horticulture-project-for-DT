9d05ccc6c5c4dc042411b5cf2b346c587536ce5e
Matte9i <alexey.thomson08@gmail.com>
2026-05-02 23:14:35 +1200

    classes and objects working

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import json
 '''code to show profitability '''
 
 class Apple:
-    def __init__(self, weight, v_quality, i_quality, s_time):
+    def __init__(self, name, weight, v_quality, i_quality, s_time):
+        self.name = name
         self.weight = weight
         self.v_quality = v_quality
         self.i_quality = i_quality
         self.s_time = s_time
 
-with open("resources/apples.json", 'r') as file:
-    info = json.load(file)
+    def __repr__(self):
+        return f"{self.name}, {self.weight} grams, {self.v_quality}, {self.i_quality}, {self.s_time}"
 
+apple_attributes = json.load(open("resources/apples.json", 'r'))
+
+Royal_Gala = Apple(**apple_attributes['Royal_gala'])
+
+print(Royal_Gala)
